42c7f4fa031144ce9a6117c74939ddd9e1886507
MD. Mehedi Hasan <53508562+MehediMubin@users.noreply.github.com>
2019-08-03 23:49:49 +0600

    Add files via upload

---
+++ ViralAdvertising.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{       
+        int n, res = 0;
+        cin >> n;
+        int num = 5;
+        for (int i = 1; i <= n; i++) {
+                int div = 0;
+                div = num / 2;
+                res += div;
+                num = div * 3;
+        }       
+        cout << res << endl;
+        
+        return 0;
+}
