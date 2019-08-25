aa5040a74a7aec824b392b035d0e2b2e9e95bc03
MD. Mehedi Hasan <53508562+MehediMubin@users.noreply.github.com>
2019-08-25 18:00:19 +0600

    Add files via upload

---
+++ LibraryFine.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main ()
+{
+	int d1, m1, y1, d2, m2, y2;
+	cin >> d1 >> m1 >> y1;
+	cin >> d2 >> m2 >> y2;
+	if (y1 == y2) {
+		if (m1 == m2) {
+			if (d1 > d2) {
+				cout << (15 * (d1 - d2)) << endl;
+			}
+			else
+				cout << 0 << endl;
+		}
+		else if (m1 > m2) {
+			cout << (500 * (m1 - m2)) << endl;
+		}
+		else
+			cout << 0 << endl;
+	}
+	else if (y1 > y2) {
+		cout << 10000 << endl;
+	}
+	else 
+		cout << 0 << endl;
+
+	return 0;
+}
