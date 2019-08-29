b4e2c5e6b42e536b44c9d831a29aa37416e7864b
MD. Mehedi Hasan <53508562+MehediMubin@users.noreply.github.com>
2019-08-29 19:46:14 +0600

    Add files via upload

---
+++ EqualizeTheArray.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+	int n;
+	cin >> n;
+	int arr[n];
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+	}
+	int counter, temp = 0;
+	for (int i = 0; i < n - 1; i++) {
+		counter = 0;
+		for (int j = i + 1; j < n; j++) {
+			if (arr[i] == arr[j])
+				counter++;
+		}
+		if (counter > temp) 
+			temp = counter;
+	}
+	temp++;
+	int res = (n - temp);
+	cout << res << endl;
+
+	return 0;
+}
